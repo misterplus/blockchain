99cbfe98f6a7235c72afe5013c67837314d77b28
misterplus <whiteplayz45@gmail.com>
2021-11-14 15:37:11 +0800

    Wallet generation

--- src/main/java/net/homework/blockchain/Test.java
+++ src/main/java/net/homework/blockchain/Test.java
@@ -1,27 +1,87 @@
 package net.homework.blockchain;
 
+import io.leonard.Base58;
 import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
 import java.util.Arrays;
 
 public class Test {
-    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(256);
-        // 256-bit private key, identification for a wallet
-        SecretKey privateKey = keyGen.generateKey();
-        // 32-byte representation
-        byte[] privateBytes = privateKey.getEncoded();
-        byte[] withPrefix = new byte[37];
-        withPrefix[0] = (byte) 0x80;
-        System.arraycopy(privateBytes, 0, withPrefix, 1, 32);
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(digest.digest(Arrays.copyOf(privateBytes, 33)));
-        System.arraycopy(hash, 0, withPrefix, 33, 4);
+
+    public static byte[] removeLeadingZero(byte[] input) {
+        if (input[0] == 0)
+            return Arrays.copyOfRange(input, 1, input.length);
+        else
+            return input;
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        Security.addProvider(new BouncyCastleProvider());
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
+        ECGenParameterSpec curve = new ECGenParameterSpec("secp256k1");
+        kpg.initialize(curve);
+        KeyPair kp = kpg.genKeyPair();
+        ECPrivateKey pri = (ECPrivateKey) kp.getPrivate();
+        ECPublicKey pub = (ECPublicKey) kp.getPublic();
+        // 0 - Private ECDSA Key
+        byte[] b0 = removeLeadingZero(pri.getS().toByteArray());
+        System.out.print("0: ");
+        System.out.println(Hex.encodeHex(b0, false));
+
+        // 1 - Public ECDSA Key
+        byte[] b1 = new byte[65];
+        b1[0] = 4;
+        System.arraycopy(removeLeadingZero(pub.getW().getAffineX().toByteArray()), 0, b1, 1, 32);
+        System.arraycopy(removeLeadingZero(pub.getW().getAffineY().toByteArray()), 0, b1, 33, 32);
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        System.out.print("1: ");
+        System.out.println(Hex.encodeHex(b1, false));
+
+        // 2 - SHA-256 hash of 1
+        byte[] b2 = sha256.digest(b1);
+        System.out.print("2: ");
+        System.out.println(Hex.encodeHex(b2, false));
+
+        MessageDigest ripemd160 = MessageDigest.getInstance("RIPEMD160");
+        // 3 - RIPEMD-160 Hash of 2
+        byte[] b3 = ripemd160.digest(b2);
+        System.out.print("3: ");
+        System.out.println(Hex.encodeHex(b3, false));
+
+        // 4 - Adding network bytes to 3
+        byte[] b4 = new byte[21];
+        System.arraycopy(b3, 0, b4, 1, 20);
+        System.out.print("4: ");
+        System.out.println(Hex.encodeHex(b4, false));
+
+        // 5 - SHA-256 hash of 4
+        byte[] b5 = sha256.digest(b4);
+        System.out.print("5: ");
+        System.out.println(Hex.encodeHex(b5, false));
+
+        // 6 - SHA-256 hash of 5
+        byte[] b6 = sha256.digest(b5);
+        System.out.print("6: ");
+        System.out.println(Hex.encodeHex(b6, false));
+
+        // 7 - First four bytes of 6
+        byte[] b7 = Arrays.copyOf(b6, 4);
+        System.out.print("7: ");
+        System.out.println(Hex.encodeHex(b7, false));
+
+        // 8 - Adding 7 at the end of 4
+        byte[] b8 = new byte[25];
+        System.arraycopy(b4, 0, b8, 0, 21);
+        System.arraycopy(b7, 0, b8, 21, 4);
+        System.out.print("8: ");
+        System.out.println(Hex.encodeHex(b8, false));
+
+        // 9 - Base58 encoding of 8
+        System.out.print("9: ");
+        System.out.println(Base58.encode(b8));
     }
 }
